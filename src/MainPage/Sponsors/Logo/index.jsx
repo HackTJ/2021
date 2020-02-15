6d2ad353d3dae9f4ed498364903f69833975133f
sumanthratna <sumanthratna@gmail.com>
2020-02-15 13:10:10 -0500

    Close #11

--- src/MainPage/Sponsors/Logo/index.jsx
+++ src/MainPage/Sponsors/Logo/index.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ReactTooltip from "react-tooltip";
 
 import "./index.css";
 
-const Logo = ({ url, name, logo }) => (
-  <>
-    <a
-      href={url}
-      className="sponsor-wrapper"
-      target="_blank"
-      rel="noopener noreferrer"
-      data-tip="data-tip"
-      data-for={name}
-    >
-      <img
-        className="sponsor-image"
-        src={logo}
-        alt={name}
-      />
-    </a>
-    <ReactTooltip id={name} place="top" type="dark" effect="solid">
-      {name}
-    </ReactTooltip>
-  </>
-);
+const Logo = ({ url, name, logo }) => {
+  const sponsorID = name.toLowerCase().replace(/ /g, "-");
+
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  return (
+    <>
+      <a
+        href={url}
+        className="sponsor-wrapper"
+        target="_blank"
+        rel="noopener noreferrer"
+        data-tip="data-tip"
+        data-for={sponsorID}
+      >
+        <img
+          id={"img-" + sponsorID}
+          className={`sponsor-image ${showTooltip ? "hover" : ""}`}
+          src={logo}
+          alt={name}
+        />
+      </a>
+      <ReactTooltip
+        id={sponsorID}
+        place="top"
+        type="dark"
+        effect="solid"
+        afterShow={evt => {
+          setShowTooltip(true);
+        }}
+        afterHide={evt => {
+          setShowTooltip(false);
+        }}
+      >
+        {name}
+      </ReactTooltip>
+    </>
+  );
+};
 
 export default Logo;
