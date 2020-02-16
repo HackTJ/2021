f709b1e2f65a79bd758646ee2d4007619260159b
sumanthratna <sumanthratna@gmail.com>
2020-02-15 22:20:49 -0500

    Add lazy loading to MainPage

--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 
-import Landing from "./Landing";
-import NavigationBar from "../NavigationBar";
-import Intro from "./Intro";
-import RegistrationInfo from "./RegistrationInfo";
-import RegistrationChoice from "../RegistrationChoice";
-import Questions from "./Questions";
-import Sponsors from "./Sponsors";
-// import Statistics from "./Statistics";
-import Schedule from "./Schedule";
-import Team from "./Team";
-import VenueMap from "./VenueMap";
-import Footer from "../Footer";
-
-import "../index.css";
+import IndeterminateLoadingIndicator from "../IndeterminateLoadingIndicator";
+const Landing = lazy(() => import("./Landing"));
+const NavigationBar = lazy(() => import("../NavigationBar"));
+const Intro = lazy(() => import("./Intro"));
+const RegistrationInfo = lazy(() => import("./RegistrationInfo"));
+const RegistrationChoice = lazy(() => import("../RegistrationChoice"));
+const Questions = lazy(() => import("./Questions"));
+const Sponsors = lazy(() => import("./Sponsors"));
+// const Statistics = lazy(() => import("./Statistics"));
+const Schedule = lazy(() => import("./Schedule"));
+const Team = lazy(() => import("./Team"));
+const VenueMap = lazy(() => import("./VenueMap"));
+const Footer = lazy(() => import("../Footer"));
 
 const MainPage = () => (
   <span className="App">
-    <Landing />
-    <NavigationBar />
-    <Intro />
-    <RegistrationInfo />
-    <RegistrationChoice />
-    <Questions />
-    <Sponsors />
-    {/*<Statistics />*/}
-    <Schedule />
-    <Team />
-    <VenueMap />
-    <Footer />
+    <Suspense fallback=<IndeterminateLoadingIndicator />>
+      <Landing />
+      <NavigationBar />
+      <Intro />
+      <RegistrationInfo />
+      <RegistrationChoice />
+      <Questions />
+      <Sponsors />
+      {/*<Statistics />*/}
+      <Schedule />
+      <Team />
+      <VenueMap />
+      <Footer />
+    </Suspense>
   </span>
 );
 
