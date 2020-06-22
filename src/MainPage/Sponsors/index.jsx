25446f5197f9f66b5b08e34739462205c9e2c5fe
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-22 14:19:16 -0400

    Use map for memoized Logo in Sponsors https://reactjs.org/docs/reconciliation.html#recursing-on-children

--- src/MainPage/Sponsors/index.jsx
+++ src/MainPage/Sponsors/index.jsx
@@ -41,106 +41,140 @@ const Sponsors: StatelessFunctionalComponent<{}> = (): Element<
         ></div>*/}
         <h1 className="section-title">Sponsors</h1>
         <div className="partner-container">
-          <Logo
-            url="https://aws.amazon.com/"
-            logo="aws.png"
-            name="Amazon Web Services"
-          />
-          <Logo url="https://www.yext.com" logo="yext.png" name="Yext" />
-          <Logo url="https://www.ensco.com" logo="ensco.png" name="Ensco" />
-          <Logo
-            url="https://cloudwaveinc.com/"
-            logo="cloudwave.png"
-            name="CloudWave"
-          />
-          <Logo
-            url="https://www.citadelsecurities.com/"
-            logo="citadelsecurities.png"
-            name="Citadel Securities"
-          />
-          <Logo
-            url="https://phone2action.com"
-            logo="phone2action.png"
-            name="Phone2Action"
-          />
-          <Logo
-            url="https://www.wolfram.com"
-            logo="wolfram.png"
-            name="Wolfram"
-          />
-          <Logo
-            url="https://www.collinsaerospace.com"
-            logo="collinsaerospace.png"
-            name="Collins Aerospace"
-          />
-          <Logo
-            url="https://www.alertfive.io"
-            logo="alertfive.png"
-            name="Alert Five"
-          />
-          <Logo url="http://razorx2.com" logo="razorx2.png" name="RazorX2" />
-          <Logo
-            url="https://www.verizonmedia.com/"
-            logo="verizonmedia.png"
-            name="Verizon Media"
-          />
-          <Logo
-            url="https://www.primamedicine.com"
-            logo="primamedicine.png"
-            name="Prima Medicine"
-          />
-          <Logo
-            url="http://hackp.ac/mlh-stickermule-hackathons"
-            logo="stickermule.png"
-            name="Sticker Mule"
-          />
-          <Logo
-            url="https://www.digitalocean.com"
-            logo="digitalocean.png"
-            name="DigitalOcean"
-          />
-          <Logo
-            url="https://www.thinkfun.com"
-            logo="thinkfun.png"
-            name="ThinkFun"
-          />
-          <Logo url="http://spontit.io" logo="spontit.png" name="Spontit" />
-          <Logo
-            url="https://ultrapress.com"
-            logo="ultrapress.png"
-            name="UltraPress"
-          />
-          <Logo
-            url="https://www.usehawkeye.com"
-            logo="hawkeye.png"
-            name="Hawkeye"
-          />
-          <Logo
-            url="https://stackoverflow.com"
-            logo="stackoverflow.png"
-            name="StackOverflow"
-          />
+          {[
+            {
+              url: "https://aws.amazon.com/",
+              logo: "aws.png",
+              name: "Amazon Web Services",
+            },
+            {
+              url: "https://www.yext.com",
+              logo: "yext.png",
+              name: "Yext",
+            },
+            {
+              url: "https://www.ensco.com",
+              logo: "ensco.png",
+              name: "Ensco",
+            },
+            {
+              url: "https://cloudwaveinc.com/",
+              logo: "cloudwave.png",
+              name: "CloudWave",
+            },
+            {
+              url: "https://www.citadelsecurities.com/",
+              logo: "citadelsecurities.png",
+              name: "Citadel Securities",
+            },
+            {
+              url: "https://phone2action.com",
+              logo: "phone2action.png",
+              name: "Phone2Action",
+            },
+            {
+              url: "https://www.wolfram.com",
+              logo: "wolfram.png",
+              name: "Wolfram",
+            },
+            {
+              url: "https://www.collinsaerospace.com",
+              logo: "collinsaerospace.png",
+              name: "Collins Aerospace",
+            },
+            {
+              url: "https://www.alertfive.io",
+              logo: "alertfive.png",
+              name: "Alert Five",
+            },
+            {
+              url: "http://razorx2.com",
+              logo: "razorx2.png",
+              name: "RazorX2",
+            },
+            {
+              url: "https://www.verizonmedia.com/",
+              logo: "verizonmedia.png",
+              name: "Verizon Media",
+            },
+            {
+              url: "https://www.primamedicine.com",
+              logo: "primamedicine.png",
+              name: "Prima Medicine",
+            },
+            {
+              url: "http://hackp.ac/mlh-stickermule-hackathons",
+              logo: "stickermule.png",
+              name: "Sticker Mule",
+            },
+            {
+              url: "https://www.digitalocean.com",
+              logo: "digitalocean.png",
+              name: "DigitalOcean",
+            },
+            {
+              url: "https://www.thinkfun.com",
+              logo: "thinkfun.png",
+              name: "ThinkFun",
+            },
+            {
+              url: "http://spontit.io",
+              logo: "spontit.png",
+              name: "Spontit",
+            },
+            {
+              url: "https://ultrapress.com",
+              logo: "ultrapress.png",
+              name: "UltraPress",
+            },
+            {
+              url: "https://www.usehawkeye.com",
+              logo: "hawkeye.png",
+              name: "Hawkeye",
+            },
+            {
+              url: "https://stackoverflow.com",
+              logo: "stackoverflow.png",
+              name: "StackOverflow",
+            },
+          ].map((company) => (
+            <Logo
+              name={company.name}
+              url={company.url}
+              logo={company.logo}
+              key={company.name}
+            ></Logo>
+          ))}
         </div>
         <p className="large">
           Can't see our sponsors? Try disabling ad block for this page.
         </p>
         <h1 className="section-title">Partners</h1>
         <div className="partner-container">
-          <Logo
-            url="https://mlh.io"
-            logo="mlh.png"
-            name="Major League Hacking"
-          />
-          <Logo
-            url="https://www.cvent.com"
-            logo="cvent.png"
-            name="Cvent"
-          />
-          <Logo
-            url="https://www.tjpartnershipfund.org"
-            logo="tjpartnershipfund.png"
-            name="TJ Partnership Fund"
-          />
+          {[
+            {
+              url: "https://mlh.io",
+              logo: "mlh.png",
+              name: "Major League Hacking",
+            },
+            {
+              url: "https://www.cvent.com",
+              logo: "cvent.png",
+              name: "Cvent",
+            },
+            {
+              url: "https://www.tjpartnershipfund.org",
+              logo: "tjpartnershipfund.png",
+              name: "TJ Partnership Fund",
+            },
+          ].map((partner) => (
+            <Logo
+              name={partner.name}
+              url={partner.url}
+              logo={partner.logo}
+              key={partner.name}
+            ></Logo>
+          ))}
         </div>
         <p className="large">
           Interested in sponsoring? Check out our{" "}
