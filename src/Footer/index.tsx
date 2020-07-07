939eee2e3ed31419fce0abc7f720c637b79c09cc
Sumanth Ratna <sumanthratna@gmail.com>
2020-07-07 13:16:20 -0400

    Move MLHTrustBadge to Footer The only suggestion from MLH is that the badge code is before </body> and in the top-right corner of the page. Both of these conditions should be satisfied. See https://mlh.io/brand-guidelines.

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,20 +1,24 @@
 import * as React from "react";
 
 import SocialLinks from "./SocialLinks";
+import MLHTrustBadge from "./MLHTrustBadge";
 
 import "./index.css";
 
 const Footer = (): JSX.Element => (
-  <section className="green register footer">
-    <div className="container">
-      <div className="register-button-block-old bottom">
-        <div className="register-text">
-          Be sure to follow our Social Media pages for updates!
+  <>
+    <section className="green register footer">
+      <div className="container">
+        <div className="register-button-block-old bottom">
+          <div className="register-text">
+            Be sure to follow our Social Media pages for updates!
+          </div>
         </div>
+        <SocialLinks />
       </div>
-      <SocialLinks />
-    </div>
-  </section>
+    </section>
+    <MLHTrustBadge />
+  </>
 );
 
 export default Footer;
--- src/NavigationBar/index.tsx
+++ src/NavigationBar/index.tsx
@@ -2,56 +2,51 @@ import * as React from "react";
 
 import { Navbar, Nav, NavLink } from "reactstrap";
 
-import MLHTrustBadge from "./MLHTrustBadge";
-
 import "./index.css";
 
 const NavigationBar = (): JSX.Element => (
   // TODO: get rid of <div style={{height: "50px"}} /> and use CSS instead
-  <>
-    <MLHTrustBadge />
-    <Navbar
-      sticky="top"
-      className="navcontainer"
-      bg="#64d861"
-      style={{
-        position: "sticky",
-        top: "0px",
-        display: "flex",
-        flexDirection: "row",
-        zIndex: 1000,
-      }}
-    >
-      <div style={{ height: "50px" }} />
-      <Nav className="nav justify-content-center">
-        <NavLink href="#intro">
-          <h1>Intro</h1>
-        </NavLink>
-        <NavLink href="#faq">
-          <h1>FAQ</h1>
-        </NavLink>
-        <NavLink href="#sponsor">
-          <h1>Sponsors</h1>
-        </NavLink>
-        {/*
+  <Navbar
+    sticky="top"
+    className="navcontainer"
+    bg="#64d861"
+    style={{
+      position: "sticky",
+      top: "0px",
+      display: "flex",
+      flexDirection: "row",
+      zIndex: 1000,
+    }}
+  >
+    <div style={{ height: "50px" }} />
+    <Nav className="nav justify-content-center">
+      <NavLink href="#intro">
+        <h1>Intro</h1>
+      </NavLink>
+      <NavLink href="#faq">
+        <h1>FAQ</h1>
+      </NavLink>
+      <NavLink href="#sponsor">
+        <h1>Sponsors</h1>
+      </NavLink>
+      {/*
         <NavLink href="#demographics">
          <h1>Demographics</h1>
         </NavLink>
         */}
-        <NavLink href="#schedule">
-          <h1>Schedule</h1>
-        </NavLink>
-        <NavLink href="#team">
-          <h1>Team</h1>
-        </NavLink>
-        {/*
+      <NavLink href="#schedule">
+        <h1>Schedule</h1>
+      </NavLink>
+      <NavLink href="#team">
+        <h1>Team</h1>
+      </NavLink>
+      {/*
         <NavLink href="#map">
          <h1>Map</h1>
         </NavLink>
         */}
-      </Nav>
-    </Navbar>
-  </>
+    </Nav>
+  </Navbar>
 );
 
 export default NavigationBar;
