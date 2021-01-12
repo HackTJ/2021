a0e06065f977a2fd199c46367db896f67a649ebf
Sumanth Ratna <2022sratna@tjhsst.edu>
2021-01-12 11:20:28 -0500

    Use CSS modules in Questions/FAQuestion

--- src/MainPage/Questions/FAQuestion/index.tsx
+++ src/MainPage/Questions/FAQuestion/index.tsx
@@ -2,7 +2,7 @@ import { useState, memo } from "react";
 import type { FunctionComponent } from "react";
 import Card from "./Card";
 
-import "./index.css";
+import styles from "./faquestion.module.css";
 
 type Props = {
   data: { question: string; answer: JSX.Element }[];
@@ -11,13 +11,13 @@ const FAQuestion: FunctionComponent<Props> = ({ data }: Props) => {
   const [selected, setSelected] = useState(-1);
 
   return (
-    <div className="wrapper">
+    <div className={`${styles.wrapper}`}>
       {data.map(
         (pair: { question: string; answer: JSX.Element }, i: number) => (
           <div
-            className={`container ${selected === i ? "selected" : ""}${
-              selected - 1 === i ? " round-bottom" : ""
-            }`}
+            className={`${styles.container} ${
+              selected === i ? styles.selected : ""
+            } ${selected - 1 === i ? styles.roundBottom : ""}`}
             key={i}
             onClick={() => {
               setSelected(selected === i ? -1 : i);
