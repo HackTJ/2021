103aa079fb0496c174457e13023c72dcc5316286
Sumanth Ratna <2022sratna@tjhsst.edu>
2021-01-17 22:52:18 -0500

    Remove <h1> tags from NavLink in NavigationBar

--- src/NavigationBar/index.tsx
+++ src/NavigationBar/index.tsx
@@ -3,44 +3,32 @@ import { Navbar, Nav, NavLink } from "reactstrap";
 import "./index.css";
 
 const NavigationBar = (): JSX.Element => (
-  // TODO: get rid of <div style={{height: "50px"}} /> and use CSS instead
   <Navbar
     sticky="top"
     className="navcontainer"
-    bg="var(--theme-primary)"
     style={{
       position: "sticky",
       top: "0px",
       display: "flex",
       flexDirection: "row",
       zIndex: 1000,
+      marginBottom: "100px",
     }}
   >
-    <div style={{ height: "50px" }} />
     <Nav className="nav justify-content-center">
-      <NavLink href="#intro">
-        <h1>Intro</h1>
-      </NavLink>
-      <NavLink href="#faq">
-        <h1>FAQ</h1>
-      </NavLink>
-      <NavLink href="#sponsor">
-        <h1>Sponsors</h1>
-      </NavLink>
+      <NavLink href="#intro">Intro</NavLink>
+      <NavLink href="#faq">FAQ</NavLink>
+      <NavLink href="#sponsor">Sponsors</NavLink>
       {/*
         <NavLink href="#demographics">
-         <h1>Demographics</h1>
+         Demographics
         </NavLink>
         */}
-      <NavLink href="#schedule">
-        <h1>Schedule</h1>
-      </NavLink>
-      <NavLink href="#team">
-        <h1>Team</h1>
-      </NavLink>
+      <NavLink href="#schedule">Schedule</NavLink>
+      <NavLink href="#team">Team</NavLink>
       {/*
         <NavLink href="#map">
-         <h1>Map</h1>
+         Map
         </NavLink>
         */}
     </Nav>
