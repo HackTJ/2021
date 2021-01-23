bdcd946effc24f12309a6ccab593028b8d037a1c
Sumanth Ratna <sumanth@hacktj.org>
2021-01-23 16:09:44 -0500

    Only enable inlineCss for react-snap for homepage
    This commit fixes an issue where running react-snap resulted in a flash 
    of unstyled content on the event site but not the homepage site.

--- src/NotFoundPage/Error404Content/index.tsx
+++ src/NotFoundPage/Error404Content/index.tsx
@@ -8,7 +8,7 @@ const Error404Content = (): JSX.Element => (
         <div className="animation-container vertical top right extra-right extra-height">
           <div className="animated" />
         </div>
-        <h1 className="section-title" style={{ marginTop: "25%" }}>
+        <h1 className="section-title" style={{ marginTop: "20vh" }}>
           Error 404
         </h1>
         <p className="large" style={{ textAlign: "center" }}>
