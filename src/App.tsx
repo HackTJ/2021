0194db2badd209f9f2ba595f29336f76127513cd
Jazon <2021jhe@tjhsst.edu>
2021-02-20 13:52:44 -0500

    Fix homepage footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import PageScrollProgress from "./PageScrollProgress";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./Footer";
+import PageScrollProgress from './PageScrollProgress';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Footer';
 
-import RegistrationPage from "./RegistrationPage";
-import HistoryPage from "./HistoryPage";
-import MainPage from "./MainPage";
-import NotFoundPage from "./NotFoundPage";
+import RegistrationPage from './RegistrationPage';
+import HistoryPage from './HistoryPage';
+import MainPage from './MainPage';
+import NotFoundPage from './NotFoundPage';
+
+import './App.css';
 
 const App = () => (
-  <>
-    <PageScrollProgress />
-    <BrowserRouter>
-      <Routes basename="/2021">
-        <Route path="/registration" element={<RegistrationPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
-  </>
+    <div className='Content'>
+        <div>
+            <PageScrollProgress />
+            <BrowserRouter>
+                <Routes basename='/2021'>
+                    <Route
+                        path='/registration'
+                        element={<RegistrationPage />}
+                    />
+                    <Route path='/history' element={<HistoryPage />} />
+                    <Route path='/' element={<MainPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+        <div className="Footer">
+            <Footer />
+        </div>
+    </div>
 );
 
 export default App;
