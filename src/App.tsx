d2258fd5e4caf4a5b7a86483e698811b03339343
Sumanth Ratna <sumanth@hacktj.org>
2021-02-20 14:28:07 -0500

    Use CSS modules in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import PageScrollProgress from './PageScrollProgress';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './Footer';
+import config from "./config";
 
-import RegistrationPage from './RegistrationPage';
-import HistoryPage from './HistoryPage';
-import MainPage from './MainPage';
-import NotFoundPage from './NotFoundPage';
+import PageScrollProgress from "./PageScrollProgress";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Footer";
 
-import './App.css';
+import RegistrationPage from "./RegistrationPage";
+import HistoryPage from "./HistoryPage";
+import MainPage from "./MainPage";
+import NotFoundPage from "./NotFoundPage";
 
-const App = () => (
-    <div className='Content'>
-        <div>
-            <PageScrollProgress />
-            <BrowserRouter>
-                <Routes basename='/2021'>
-                    <Route
-                        path='/registration'
-                        element={<RegistrationPage />}
-                    />
-                    <Route path='/history' element={<HistoryPage />} />
-                    <Route path='/' element={<MainPage />} />
-                    <Route path='*' element={<NotFoundPage />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-        <div className="Footer">
-            <Footer />
-        </div>
+import styles from "./App.module.css";
+
+const App = () => {
+  const currentTime = new Date();
+
+  const registrationIsOpen = config.registration.startDate <= currentTime && currentTime < config.event.startDate;
+  return <div className={styles.content}>
+    <div>
+      <PageScrollProgress />
+      <BrowserRouter>
+        <Routes basename="/2021">
+          {registrationIsOpen && <Route path="/registration" element={<RegistrationPage />} />}
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-);
+    <Footer />
+  </div>
+};
 
 export default App;
