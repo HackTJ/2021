e695053179ad28b236a15713e3e77063344128e7
Sumanth Ratna <sumanth@hacktj.org>
2021-02-22 14:11:38 -0500

    Fix App test and ignore react-snap exit code

---
+++ src/setupTests.js
@@ -0,0 +1,25 @@
+// react-testing-library renders components to document.body;
+// this adds jest-dom's custom assertions
+import "@testing-library/jest-dom";
+
+// so that `Object.freeze(config)` doesn't actually do anything.
+// this allows us to configure variables like the event start and end times,
+// along with the registration start and end times. i.e., we can monkey-patch
+// `config` by overriding `Object.freeze` and setting it to a no-op.
+Object.freeze = function (a) {
+  return;
+};
+
+// TODO: this doesn't work; the timezone remains at EST
+beforeAll(() => {
+  process.env.TZ = "PST";
+});
+
+// import { DateTime } from "luxon";
+//
+// describe('timezone', () => {
+//   it('should always be PST when testing', () => {
+//     expect(new Date().getTimezoneOffset()).toBe(8 * 60);
+//     expect(DateTime.now().zone.name).toBe("America/Los_Angeles");
+//   });
+// });
